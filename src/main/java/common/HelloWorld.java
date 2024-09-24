13e54531452d38926e72e8212f9d1e0d9f166162
mayu0112 <mayu.chauhan@gmail.com>
2024-09-24 19:15:55 +0530

    Initial commit

---
+++ src/main/java/common/HelloWorld.java
@@ -0,0 +1,16 @@
+package common;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("*Line 1");
+        System.out.println("*Hello World*");
+        System.out.println("Line 2");
+    }
+
+    public void test(){
+        System.out.println("new method");
+
+    }
+}
